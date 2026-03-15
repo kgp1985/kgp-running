d60a8281128e6d2c117ad5930f58e08fa954e1eb
Kyle Pederson <kylepederson@Kyles-MacBook-Air.local>
2026-03-15 13:56:48 -0400

    Add watch integration, profile page, community feed, privacy policy

--- supabase/functions/garmin-oauth-start/index.ts
+++ supabase/functions/garmin-oauth-start/index.ts
@@ -12,7 +12,6 @@
 
 import { serve } from 'https://deno.land/std@0.168.0/http/server.ts'
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-import { hmac } from 'https://deno.land/x/hmac@v2.0.1/mod.ts'
 
 const GARMIN_REQUEST_TOKEN_URL = 'https://connectapi.garmin.com/oauth-service/oauth/request_token'
 const GARMIN_AUTHORIZE_URL     = 'https://connect.garmin.com/oauthConfirm'
